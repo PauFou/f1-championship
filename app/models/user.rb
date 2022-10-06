527e58380fc3fe13414657394d27f8b841aca192
Paul Fournier <pfournier597@gmail.com>
2022-10-06 16:46:41 +0200

    quelques fondations, mise en place de la gem devise (bundle install nécessaire). Cependant problème avec la migration, je ne sais pas d où cela provient

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true, length: { maximum: 50 }
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
